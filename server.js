e8bd0fe20157139723cac8c1abfec1d63691db67
Ashlyn Baum <a@ashlynbaum.com>
2015-02-23 13:36:18 +1300

    respoinding to pull request comments ref #1

--- server.js
+++ server.js
@@ -3,15 +3,7 @@ var express = require("express")
 var app = express()
 
 app.get('/hello/:name', function(req, res){
-	console.log(req)
 	res.send('Hello ' + req.params.name)
 })
 
-var server = app.listen(process.env.PORT || 3000, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
-})
+var server = app.listen(process.env.PORT || 3000)
