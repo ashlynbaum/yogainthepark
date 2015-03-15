20a78176ead71179285d04cc2a261865d3099230
Ashlyn Baum <a@ashlynbaum.com>
2015-03-16 10:05:41 +1300

    add api text file documenting api for project.
    Ref #2

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-var express = require("express")
-
-var app = express()
+var app = require('express')()
+  // , API = require('json-api');
 
 app.get('/hello/:name', function(req, res){
-	res.send('Hello ' + req.params.name)
+	res.send('Hello ' + req.params.name);
 })
 
-var server = app.listen(process.env.PORT || 3000)
+var server = app.listen(process.env.PORT || 3000);
