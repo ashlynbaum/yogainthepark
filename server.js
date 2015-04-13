25bb751e4d166588b77b676bd0d8cff6c5071fd0
Ashlyn Baum <a@ashlynbaum.com>
2015-04-13 18:46:20 +1200

    implement create events #8

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var mocks = require('./mocks');
 
 var db = new mocks.Database();
 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
 app.get('/events', function(req, res){
   db.events().all(function(err, events) {
     res.send(events);
@@ -14,6 +18,12 @@ app.get('/events', function(req, res){
 app.get('/events/:id', function(req, res){
   db.events().find(req.params.id, function(err, event) {
     res.send(event);
+  });
+});
+
+app.post('/events', function(req, res){
+  db.events().insert( req.body, function(err, events) {
+    res.status(201).send(req.body);
   });
 });
 
