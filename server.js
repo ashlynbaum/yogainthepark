e50877bdf1435e21289c41e3113c264789c96407
Ashlyn Baum <a@ashlynbaum.com>
2015-04-13 14:40:25 +1200

    reindent using spaces

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ var mocks = require('./mocks');
 var db = new mocks.Database();
 
 app.get('/events', function(req, res){
-	db.events().all(function(err, events) {
-	  res.send(events);
-	});
+  db.events().all(function(err, events) {
+    res.send(events);
+  });
 });
 
 var server = app.listen(process.env.PORT || 3000);
