648869f04447d4061d3d02456fd808cf9fa309bc
Ashlyn Baum <a@ashlynbaum.com>
2015-05-08 16:19:10 +1200

    Fix bug: crash when authorization header is not sent

--- server.js
+++ server.js
@@ -226,20 +226,24 @@ MongoClient.connect(url, function (err, db) {
     //Basic Auth Login
     app.get('/', function(req, res){
       var basicAuthUser = basicAuth(req);
-      usersCollection.findOne( {email: basicAuthUser.name}, function(err, user){
-        if (!user){
-          res.status(403).end()
-        } else {
-          bcrypt.compare( basicAuthUser.pass , user.encryptedPassword, function(err, isSame) {
-            if (isSame){
-              user = formatUser(user);
-              res.status(200).send({auth_token: user.authToken});
-            } else{
-              res.status(403).end()
-            }
-          });
-        }
-      });
+      if (!basicAuthUser) {
+        return res.status(403).end()
+      } else {
+        usersCollection.findOne( {email: basicAuthUser.name}, function(err, user){
+          if (!user){
+            res.status(403).end()
+          } else {
+            bcrypt.compare( basicAuthUser.pass , user.encryptedPassword, function(err, isSame) {
+              if (isSame){
+                user = formatUser(user);
+                res.status(200).send({auth_token: user.authToken});
+              } else{
+                res.status(403).end()
+              }
+            });
+          }
+        });
+      }
     });
 
   }
