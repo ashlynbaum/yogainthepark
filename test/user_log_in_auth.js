3d2e088feb69d25f55f725d8280cad6ad4fe9888
Ashlyn Baum <a@ashlynbaum.com>
2015-06-05 16:10:07 +1200

    Fix style of tests

--- test/user_log_in_auth.js
+++ test/user_log_in_auth.js
@@ -1,4 +1,5 @@
-var assert = require('chai').assert, expect = require('chai').expect;
+var assert = require('chai').assert;
+var expect = require('chai').expect;
 var request = require('supertest');
 var app = require('../server');
 
@@ -6,86 +7,86 @@ before(function(done) {
   app.on('ready', done);
 });
 
-var db
+var db;
 // define database
-before(function(done){
- var MongoClient = require('mongodb').MongoClient
- // Connection URL for database
- var url = ( process.env.MONGOLAB_URI  || 'mongodb://localhost:27017/yoga' );
- // hook to database before each hook
- MongoClient.connect(url, function (err, database) {
-  db = database;
-  done();
- })
-})
+before(function(done) {
+  var MongoClient = require('mongodb').MongoClient;
+  // Connection URL for database
+  var url = ( process.env.MONGOLAB_URI || 'mongodb://localhost:27017/yoga' );
+  // hook to database before each hook
+  MongoClient.connect(url, function(err, database) {
+    db = database;
+    done();
+  });
+});
 
 
 beforeEach(function(done) {
-  db.dropDatabase(function(){
+  db.dropDatabase(function() {
     done();
-  })
-})
+  });
+});
 
-describe('users basic auth login', function(){
-  describe('GET /test',function(){
-    it('should get 200 for test endpoint', function(done){
+describe('users basic auth login', function() {
+  describe('GET /test', function() {
+    it('should get 200 for test endpoint', function(done) {
       request(app)
         .get('/test')
         .expect(200)
-        .end(done)
-    })
-  })
-  describe('Get /',function(){
+        .end(done);
+    });
+  });
+  describe('Get /' ,function() {
     var signupRequest = function() {
       return request(app)
         .post('/signup')
-        .send({"email": "a@example.com", "password": "sample"});
+        .send({'email': 'a@example.com', 'password': 'sample'});
     };
-    it('should recieve a 200 and user authentication token', function(done){
+    it('should recieve a 200 and user authentication token', function(done) {
       signupRequest()
         .expect(200)
-        .end(function(){
+        .end(function() {
           request(app)
             .get('/')
             .auth('a@example.com', 'sample')
             .expect(200)
             .expect(/[a-f\d]{24}/)
-            .end(done)
-        })
-    })
-    it('should recieve a 200 and user authentication token', function(done){
+            .end(done);
+        });
+    });
+    it('should recieve a 200 and user authentication token', function(done) {
       signupRequest()
         .expect(200)
-        .end(function(){
+        .end(function() {
           request(app)
             .get('/')
             .auth('a@example.com', 'sample')
             .expect(200)
             .expect(/[a-f\d]{24}/)
-            .end(done)
-        })
-    })
+            .end(done);
+        });
+    });
 
-    it('should recieve a 403 if user does not exist', function(done){
+    it('should recieve a 403 if user does not exist', function(done) {
       signupRequest()
         .expect(200)
-        .end(function(){
+        .end(function() {
           request(app)
             .get('/')
             .auth('nonExistantUser@example.com', 'sample')
-            .expect(403,done)
-        })
-    })
+            .expect(403, done);
+        });
+    });
 
-    it('should recieve a 403 and user password does not match', function(done){
+    it('should recieve a 403 and user password does not match', function(done) {
       signupRequest()
         .expect(200)
-        .end(function(){
+        .end(function() {
           request(app)
             .get('/')
             .auth('a@example.com', 'newpassword')
-            .expect(403,done)
-        })
-    })
-  })
-})
+            .expect(403, done);
+        });
+    });
+  });
+});
--- test/user_sign_up.js
+++ test/user_sign_up.js
@@ -4,70 +4,65 @@ var app = require('../server');
 
 before(function(done) {
   app.on('ready', done);
-
 });
 
-var db
+var db;
 // define database
-before(function(done){
- var MongoClient = require('mongodb').MongoClient
- // Connection URL for database
- var url = ( process.env.MONGOLAB_URI  || 'mongodb://localhost:27017/yoga' );
- // hook to database before each hook
- MongoClient.connect(url, function (err, database) {
-  db = database;
-  done();
- })
-})
+before(function(done) {
+  var MongoClient = require('mongodb').MongoClient;
+  // Connection URL for database
+  var url = ( process.env.MONGOLAB_URI || 'mongodb://localhost:27017/yoga' );
+  // hook to database before each hook
+  MongoClient.connect(url, function(err, database) {
+    db = database;
+    done();
+  });
+});
 
 
 beforeEach(function(done) {
-  db.dropDatabase(function(){
+  db.dropDatabase(function() {
     done();
-  })
-})
+  });
+});
 
-describe('authentication of users', function(){
-  describe('GET /test',function(){
-    it('should get 200 for test endpoint', function(done){
+describe('authentication of users', function() {
+  describe('GET /test', function() {
+    it('should get 200 for test endpoint', function(done) {
       request(app)
         .get('/test')
         .expect(200)
-        .end(done)
-    })
-  })
-  describe('POST /signup',function(){
+        .end(done);
+    });
+  });
+  describe('POST /signup', function() {
     var signupRequest = function() {
       return request(app)
         .post('/signup')
-        .send({"email": "a@example.com", "password": "sample"});
+        .send({'email': 'a@example.com', 'password': 'sample'});
     };
-    it('should recieve a 200 and authentication token', function(done){
+    it('should recieve a 200 and authentication token', function(done) {
       signupRequest()
         .expect(200)
         .expect(/^authToken is [a-f\d]{64}$/)
-        .end(done)
-      // Send username and password
-      // recieve '200'
-      // recieve authorization token
-      // remove username from database
-    })
-    it('should recieve a 422 when emails already exists in database', function(done){
+        .end(done);
+    });
+    it('should recieve a 422 when emails already exists in database', function(done) {
       signupRequest()
         .expect(200)
-        .end(function(){
+        .end(function() {
           signupRequest()
             .expect(422)
-            .end(done)
-        })
-    })
-    it('should fail if email is not valid', function(done){
+            .end(done);
+        });
+    });
+    it('should fail if email is not valid', function(done) {
       request(app)
         .post('/signup')
-        .send({"email": "aaaaaa.com", "password": "sample"})
+        .send({'email': 'aaaaaa.com', 'password': 'sample'})
         .expect(422)
-        .expect("Invalid email")
-        .end(done)
-    })
-  })
-})
+        .expect('Invalid email')
+        .end(done);
+    });
+  });
+});
