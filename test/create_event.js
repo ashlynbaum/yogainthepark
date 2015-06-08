386d1bc2dc587ed8941cf10a649eebb1f9859415
Ashlyn Baum <a@ashlynbaum.com>
2015-06-08 23:01:59 +1200

    Begin test for creating events

---
+++ test/create_event.js
@@ -0,0 +1,38 @@
+var initApp = require('./helpers/init_app');
+
+describe('Events', function() {
+  initApp.initDb();
+
+  describe('GET /test', function() {
+    it('should get 200 for test endpoint', function(done) {
+      request(app)
+        .get('/test')
+        .expect(200)
+        .end(done);
+    });
+  });
+
+
+  describe('POST /events', function() {
+    describe('when authorized', function() {
+      var signupRequest = function() {
+        return request(app)
+          .post('/signup')
+          .send({'email': 'a@example.com', 'password': 'sample'})
+          .expect(200);
+      };
+      it('should return 201 and the formated event', function(done) {
+        signupRequest()
+          .end(function(err, res) {
+            request(app)
+              .post('/events')
+              .set('X-Auth-Token', res.body.authToken)
+              .send({'title': 'first example event'})
+              .expect(201)
+              .expect('Content-Type', /json/)
+              .end(done);
+          });
+      });
+    });
+  });
+});
