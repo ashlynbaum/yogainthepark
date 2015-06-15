791cd5d697fc442aec5a3d0984a545e23ea14182
Ashlyn Baum <a@ashlynbaum.com>
2015-06-16 11:22:42 +1200

    Refactor create event test

--- test/create_event.js
+++ test/create_event.js
@@ -1,11 +1,7 @@
 var chai = require('chai');
-var assert = chai.assert;
 var expect = chai.expect;
-var should = chai.should();
-var request = require('supertest');
-var server = require('../server');
-var initApp = require('./helpers/init_db');
 var databaseCleaner = require('./helpers/database_cleaner');
+var requestAppWrapper = require('./helpers/request_app');
 
 describe('Events', function() {
   var cleaner = databaseCleaner();
@@ -13,14 +9,14 @@ describe('Events', function() {
   before(cleaner.init);
   beforeEach(cleaner.clean);
 
-  var context = {};
+  var requestApp = requestAppWrapper();
 
-  initApp.initDb(context);
+  before(requestApp.startApp);
 
-  describe('POST /events', function() {
-    describe('when authorized', function() {
+  describe('create event', function() {
+    context('when authorized', function() {
       var signupRequest = function() {
-        return request(context.app)
+        return requestApp.r()
           .post('/signup')
           .send({'email': 'a@example.com', 'password': 'sample'})
           .expect(200);
@@ -29,23 +25,23 @@ describe('Events', function() {
         signupRequest()
           .end(function(err, res) {
             if (err) { return done(err); }
-            request(context.app)
+            requestApp.r()
               .post('/events')
               .set('Authorization', 'Token ' + res.body.authToken)
               .send({'title': 'first example event'})
               .expect(201)
               .expect('Content-Type', /json/)
               .end( function(err, res) {
-                if (err) { return done(err) }
+                if (err) { return done(err); }
                 expect(res.body).to.be.an('object').and.to.have.property('title', 'first example event');
                 done();
               });
           });
       });
     });
-    describe('when not authorized', function() {
+    context('when not authorized', function() {
       it('should return 401 unauthorized', function(done) {
-        request(context.app)
+        requestApp.r()
           .post('/events')
           .expect(401)
           .end(done);
