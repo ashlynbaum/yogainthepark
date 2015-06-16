d9c611092de20056490bcc52a24700e3ce127d9f
Ashlyn Baum <a@ashlynbaum.com>
2015-06-16 12:18:06 +1200

    Refactor show event test, remove unused variables

--- test/show_events.js
+++ test/show_events.js
@@ -14,27 +14,18 @@ describe('Events', function() {
   before(requestApp.startApp);
 
   describe('Show events', function() {
-    var authToken;
-    var eventID;
-
     var createEvent = function(email, title, done) {
       requestApp.r()
         .post('/signup')
         .send({'email': email, 'password': 'sample'})
         .end(function(err, res) {
           if (err) { return done(err); }
-          authToken = res.body.authToken;
 
           requestApp.r()
             .post('/events')
             .set('Authorization', 'Token ' + res.body.authToken)
             .send({'title': title + ' example event'})
-            .end(function(err, eventResponse) {
-              if (err) return done(err);
-
-              eventID = eventResponse.body.id;
-              done();
-            });
+            .end(done);
         });
     };
     beforeEach(function(done) {
@@ -44,21 +35,18 @@ describe('Events', function() {
       createEvent('a2@example.com', 'Second', done);
     });
 
-    // beforeEach(createEvent('a2@example.com', 'Second'));
     describe('response', function() {
-      context('no authentication required', function() {
-        it('should list all events', function(done) {
-          requestApp.r()
-            .get('/events')
-            .expect('Content-Type', /json/)
-            .end(function(err, res) {
-              if (err) { return done(err); }
-              expect(res.body).to.be.an('array').to.have.length(2);
-              expect(res.body[0]).to.have.deep.property('title', 'First example event');
-              expect(res.body[1]).to.have.deep.property('title', 'Second example event');
-              done();
-            });
-        });
+      it('should list all events', function(done) {
+        requestApp.r()
+          .get('/events')
+          .expect('Content-Type', /json/)
+          .end(function(err, res) {
+            if (err) { return done(err); }
+            expect(res.body).to.be.an('array').to.have.length(2);
+            expect(res.body[0]).to.have.deep.property('title', 'First example event');
+            expect(res.body[1]).to.have.deep.property('title', 'Second example event');
+            done();
+          });
       });
     });
   });
